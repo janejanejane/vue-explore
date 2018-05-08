6b3a419ef0144822fc5e10d1a01c2c2d63bd2447
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2018-05-08 15:24:02 +1200

    Intro vue.js from Vue Mastery

---
+++ main.js
@@ -0,0 +1,43 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        brand: 'Vue Mastery',
+        product: 'Socks',
+        selectedVariant: 0,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green',
+                variantImage: './assets/socks-green.jpg',
+                variantQuantity: 10
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue',
+                variantImage: './assets/socks-blue.jpg',
+                variantQuantity: 0
+            }
+        ],
+        cart: 0
+    },
+    methods: {
+        addToCart: function() {
+            this.cart += 1
+        },
+        updateProduct: function(index) {
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title: function() {
+            return this.brand + ' ' + this.product
+        },
+        image: function() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock: function() {
+            return this.variants[this.selectedVariant].variantQuantity
+        }
+    }
+})
